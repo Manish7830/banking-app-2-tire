43ef4abd2f0e061f230efdec347e3fbe14bcf96f
Manish7830 <ms2500287@gmail.com>
2026-07-07 06:11:12 +0530

    update

--- smoke_test.py
+++ smoke_test.py
@@ -1,11 +1,11 @@
-import boto3, os, sys
-client=boto3.client("ssm",region_name="us-east-1")
+import boto3 ,os,sys
 import pymysql
+client=boto3.client("ssm",region_name="us-east-1")
 
 params={
     os.path.basename(p["Name"]):p["Value"]
     for p in client.get_parameters_by_path(
-        Path="/application/banking/",
+        Path="/application/banking",
         WithDecryption=True)["Parameters"]
 }
 
@@ -13,7 +13,10 @@ required=["DB_HOST","DB_NAME","DB_USER","DB_PASSWORD","DB_PORT"]
 missing=[k for k in required if k not in params]
 
 for k in required:
-    print(f"{k} : { "✅" if k in params else '❌'}")
+    if k in params:
+        print(k , "✅")
+    else:
+        print(k , "❌")
 
 if missing:
     print(f"Failed : {missing}")
@@ -36,10 +39,10 @@ try:
     tables=[row[0] for row in cur.fetchall()]
     connection.close()
     print(f"{params["DB_NAME"]}")
-    print(f"Table :{tables}")
+    print(f"Table : {tables}")
 
 except Exception as e:
-    print("DB ERROR ❌ :",e)
+    print("DB ERROR ❌: ",e)
     sys.exit(1)
 
 print("✅ Smoke test Done")
